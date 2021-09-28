0a268d9fdb7ad273c0cfe1612b15228caead5073
Arunsh93 <Arunhosur93@gmail.com>
2021-09-28 16:39:10 +0530

    Welcome message to Address Book System

---
+++ AddressBook/AddressMainClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBook
+{
+    class AddressMainClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book System");
+        }
+    }
+}
